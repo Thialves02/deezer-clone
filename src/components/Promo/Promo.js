1ffa1d173cb25a216d34364cb5980932dab70875
Thiago <thiago.ralves02@gmail.com>
2021-12-29 20:49:55 -0300

    feat: promo component started

---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Promo.css'
+
+export default function Promo() {
+    return (
+        <div className="container-Promo">
+            <h1>Try Deezer Premium R$ 0,90/month for 2 months</h1>
+            <button>SUBSCRIBE</button>
+        </div>
+    )
+}
