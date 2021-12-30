3320dfe42ac8f7d144b6adb6be7b397cf66b69ac
Thiago <thiago.ralves02@gmail.com>
2021-12-30 20:02:37 -0300

    feat: logic of play/pause added

--- src/components/Song/Song.js
+++ src/components/Song/Song.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Song.css'
 import play from '../../assets/images/play.png'
+import pause from '../../assets/images/pause.png'
 import fav from '../../assets/images/fav.png'
 import full from '../../assets/images/full.png'
+import {Howl, Howler} from 'howler';
 
 export default function Song({cover,title,artist,duration,fullSong,preview}) {
+    const [click, setClick] = useState(false)
+    const [playing,setPlaying] = useState(false)
+
+    /* const handleClick = () =>{
+        setClick(!click)
+        console.log('a')
+    } */
+
+    let audio = new Audio(preview)
+
+    const start = () => {
+        if (click){
+            audio.play()
+        }
+        else{
+            audio.pause()
+        }
+        
+        setClick(!click)
+    }
     return (
         <>
+        
         <div className="container-song">
-            <img src={cover}/>
-            <div className="play">
-                <img src={play}/>
+            <img src={cover} />
+            <div className={click ? 'pause' : 'play'} >
+                <img src={click ? pause : play} onClick={start} />
             </div>
-            <div className="fav">
+            {/* <audio src={preview} controls={true}  onClick={handleClick}/> */}
+            <div className="fav" >
                 <img src={fav}/>
             </div>
             <a href={fullSong} target="_blank">
