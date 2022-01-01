08939bda1295ec138ac6ffda96d84a423defb755
Thiago <thiago.ralves02@gmail.com>
2022-01-01 12:32:36 -0300

    feat: new import

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import LateralMenu from './components/LateralMenu/LateralMenu'
 import Menu from './components/Menu/Menu'
 import CtxApp from './context/CtxApp'
 import Index from './pages/Index'
 import Favorites from './pages/Favorites'
-import Menus from './components/Menus/Menus';
 
 export default function App() {
     return (
         <>
         <CtxApp>
             <Router>
-                <Menus/>
+                <Menu/>
                 <Switch>
                     <Route path='/' exact={true} component ={Index}/>
                     <Route path='/favorites' exact={true} component={Favorites}/>
